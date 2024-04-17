f5afb5593c5068ee89784e155029a7916d3c371f
richardyu <527198700@qq.com>
2024-04-18 00:25:06 +0800

    [fix] xx

--- app/page.tsx
+++ app/page.tsx
@@ -17,7 +17,7 @@ export default function Page() {
           <p className={`${lusitana.className} text-xl text-gray-800 md:text-3xl md:leading-normal`}>
             <strong>Welcome to Acme.</strong> This is the example for the{' '}
             <a href="https://nextjs.org/learn/" className="text-red-500">
-            <h1 className="text-blue-500">I'm blue!</h1>
+            {/* <h1 className="text-blue-500">I'm blue!</h1> */}
               Next.js Learn Course
             </a>
             , brought to you by Vercel.
